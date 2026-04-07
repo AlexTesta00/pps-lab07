d7ae3491abd38a90c1cb18a280427e7358b7f97d
Alex Testa <alex.testa@studio.unibo.it>
2026-04-07 12:49:32 +0200

    feat: Finish exercise 1 - def charParser method - def NotTwoConsecutive method - define NotTwoConsecutiveParser

--- src/main/scala/ex1/Parser.scala
+++ src/main/scala/ex1/Parser.scala
@@ -1,5 +1,7 @@
 package ex1
 
+import ex1.Parsers.charParser
+
 /** Consider the Parser example shown in previous lesson. Analogously to
   * NonEmpty, create a mixin NotTwoConsecutive, which adds the idea that one
   * cannot parse two consecutive elements which are equal. Use it (as a mixin)
@@ -14,7 +16,8 @@ abstract class Parser[T]:
     (seq forall parse) & end // note &, not &&
 
 object Parsers:
-  val todo = ??? // put the extensions here..
+    extension (s: String)
+      def charParser(): Parser[Char] = new BasicParser(s.toSet)
 class BasicParser(chars: Set[Char]) extends Parser[Char]:
   override def parse(t: Char): Boolean = chars.contains(t)
   override def end: Boolean = true
@@ -26,16 +29,19 @@ trait NonEmpty[T] extends Parser[T]:
     super.parse(t) // who is super??
   abstract override def end: Boolean = !empty && super.end
 
-class NonEmptyParser(chars: Set[Char])
-    extends BasicParser(chars)
-    with NonEmpty[Char]
+class NonEmptyParser(chars: Set[Char]) extends BasicParser(chars) with NonEmpty[Char]
 
 trait NotTwoConsecutive[T] extends Parser[T]:
-  val todo = ???
-// ???
+  private[this] var last: Option[T] = None
 
-class NotTwoConsecutiveParser(chars: Set[Char])
-    extends BasicParser(chars) // with ????
+  abstract override def parse(t: T): Boolean =
+    val ok = !last.contains(t)
+    last = Some(t)
+    ok && super.parse(t)
+
+  abstract override def end: Boolean = super.end
+
+class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) with NotTwoConsecutive[Char]
 
 @main def checkParsers(): Unit =
   def parser = new BasicParser(Set('a', 'b', 'c'))
@@ -64,7 +70,7 @@ class NotTwoConsecutiveParser(chars: Set[Char])
   println(parserNTCNE.parseAll("XYYZ".toList)) // false
   println(parserNTCNE.parseAll("".toList)) // false
 
-  def sparser: Parser[Char] = ??? // "abc".charParser()
+  def sparser: Parser[Char] = "abc".charParser()
   println(sparser.parseAll("aabc".toList)) // true
   println(sparser.parseAll("aabcdc".toList)) // false
   println(sparser.parseAll("".toList)) // true
